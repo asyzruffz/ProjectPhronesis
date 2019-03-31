f4f7209e2320178579f0d6b467efecfb462622d5
Muhammad Asyraf bin Ibrahim <asyzruffz@gmail.com>
2019-03-31 19:05:00 +0800

    Added shader autocompile post-build

---
+++ TestGame/Resources/Shaders/compileshader.bat
@@ -0,0 +1,10 @@
+@echo off
+rem Post-Build Event:   call "$(ProjectDir)Resources\Shaders\compileshader.bat" "$(ProjectDir)$(IntDir)CompiledShaders\\" "$(OutDir)Shaders\\"
+set OLDDIR=%CD%
+cd /d "%~dp0"
+if not exist "%1" mkdir "%1"
+del /s /f /q "%1*.*" >nul
+for /r %%i in (*.vert, *.tesc, *.tese, *.geom, *.frag, *.comp) do "%VULKAN_SDK%/Bin/glslangValidator.exe" -V "%%i" -o "%1%%~nxi.spv"
+if not exist "%2" mkdir "%2"
+for %%f in (%~dp1*) do fc %%f "%2%%~nxf" >nul 2>&1 || copy /y %%f "%2%%~nxf"
+cd /d "%OLDDIR%"
